026650036d490db0aec09a893efae65cd008c909
FNU LNU <sonne@bulibadmj.onmicrosoft.com>
2026-03-23 08:26:16 +0530

    Fixed SQLAlchemy postgres URL compatibility

--- backend/app/database.py
+++ backend/app/database.py
@@ -13,6 +13,10 @@ if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
 else:
+    # Fix for SQLAlchemy not supporting 'postgres://' (standard in many cloud providers)
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
